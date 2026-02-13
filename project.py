e48638a8bc79aabc8878347e3bdf54f40ea048e7
Vaishnavi RS Nair <vsn-LBS-sail@users.noreply.github.com>
2026-02-13 15:15:00 +0530

    connect_check,sec_eng innov and main functions added

--- project.py
+++ project.py
@@ -1,8 +1,19 @@
+def main():
+    # Main menu to control the program.
+
 def ip_analysis():
     # To analyse an IP address with a subnet mask.
     pass
 
 def connect_check():
-    # Tocheck if two devices can communicate.
+    # To check if two devices can communicate.
+    pass
+
+def sec_eng():
+    # To apply secuity rules on top of the community.
+    pass
+
+def innov():
+    # New Feature
     pass
 
